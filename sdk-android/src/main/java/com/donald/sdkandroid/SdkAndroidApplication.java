5f7420697a82edc2986e82406fa708e902ec1a1b
Donald <448641125@qq.com>
2022-06-25 23:50:16 +0800

    Update: udpate sdk-android application

--- sdk-android/src/main/java/com/donald/sdkandroid/SdkAndroidApplication.java
+++ sdk-android/src/main/java/com/donald/sdkandroid/SdkAndroidApplication.java
@@ -1,13 +1,17 @@
 package com.donald.sdkandroid;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SdkAndroidApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SdkAndroidApplication.class, args);
-    }
 
+        SpringApplication application = new SpringApplication(SdkAndroidApplication.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+
+        application.run(args);
+    }
 }

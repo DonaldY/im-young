51d1dcadd16cee1a77fcb5060c5f26b71a50d2b2
Donald <448641125@qq.com>
2022-08-15 23:59:36 +0800

    Update: update sdk tcpClient

--- sdk-android/src/main/java/com/donald/sdk/tcp/TcpClient.java
+++ sdk-android/src/main/java/com/donald/sdk/tcp/TcpClient.java
@@ -1,8 +1,13 @@
 package com.donald.sdk.tcp;
 
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+
 /**
  * @author donald
  * @date 2022/08/14
  */
 public class TcpClient {
+
+    private static final EventLoopGroup threadGroup = new NioEventLoopGroup();
 }
